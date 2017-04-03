6578bbda30bb3eed8dde67ef4bf4e972c063d837
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-04-03 15:42:39 -0700

    added basic math functions for both standard and array

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(one:Int,two:Int) -> Int {
+    return one + two
+}
+
+func sub(one:Int,two:Int) -> Int{
+        return one - two
+}
+
+func mult(one:Int,two:Int) -> Int {
+    return one * two
+}
+
+func div(one:Int,two:Int) -> Int {
+    return one / two
+}
+
+func addElements(list:[Int]) -> Int{
+    var total = 0
+    for value in list {
+        total += value
+    }
+    return total
+}
+
+func multElements(list:[Int]) -> Int{
+    var total = 1
+    for value in list {
+        total *= value
+    }
+    return total
+}
+
+func countElements(list:[Int]) -> Int {
+    return list.count
+}
+
+func avgofElements(list:[Int]) -> Int {
+    var total = 0
+    for value in list {
+        total += value
+    }
+    return total / list.count
+
+
+
 
